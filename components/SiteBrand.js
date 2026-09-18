462b883ae9c3816e071e9b714eaca4e80ab56c51
Josh <securesovereigns@pm.me>
2026-09-18 11:16:31 -0400

    Keep the header and footer light; do not restyle them as a dark theme.

--- components/SiteBrand.js
+++ components/SiteBrand.js
@@ -7,19 +7,13 @@ export default function SiteBrand({ className = "", onClick }) {
       className={`site-brand${className ? ` ${className}` : ""}`}
       onClick={onClick}
     >
-      <picture>
-        <source
-          srcSet="/assets/logo-white.png"
-          media="(prefers-color-scheme: dark)"
-        />
-        <img
-          src="/assets/logo.png"
-          alt="BTCDecoded"
-          className="site-brand__img"
-          width={870}
-          height={285}
-        />
-      </picture>
+      <img
+        src="/assets/logo.png"
+        alt="BTCDecoded"
+        className="site-brand__img"
+        width={870}
+        height={285}
+      />
     </Link>
   );
 }
